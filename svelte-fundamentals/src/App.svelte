e308d36e937f42b413b4b1ed68fee5d60238385a
Nikita Sukhachev <traed.traed@mail.ru>
2022-08-07 21:19:32 +0300

    lesson-13

---
+++ svelte-fundamentals/src/App.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  let count = 0;
+
+  const handleClick = (diff: number) => (count += diff);
+
+  interface IFormValues {
+    name: string;
+    age: number | null;
+    profileSummary: string;
+    country: string;
+    jobLocations: string[];
+  }
+
+  const formValues: IFormValues = {
+    name: '',
+    age: null,
+    profileSummary: '',
+    country: '',
+    jobLocations: [],
+  };
+</script>
+
+<main>
+  <div>
+    <button on:click={() => handleClick(-1)}>decrease count {count}</button>
+    <button on:click={() => handleClick(1)}> increase count {count}</button>
+    <button on:click={(e) => console.log(e)}>show type</button>
+  </div>
+  <div>
+    {JSON.stringify(formValues)}
+  </div>
+  <div>
+    <form>
+      <div>
+        <label for="name">Name</label>
+        <input type="text" id="name" bind:value={formValues.name} />
+      </div>
+      <div>
+        <label for="age">Age</label>
+        <input type="number" id="age" bind:value={formValues.age} />
+      </div>
+      <div>
+        <label for="profile">Profile</label>
+        <textarea id="profile" bind:value={formValues.profileSummary} />
+      </div>
+      <div>
+        <label for="country">Country</label>
+        <select id="country" bind:value={formValues.country}>
+          <option value="">Select a country</option>
+          <option value="VN">Việt Nam</option>
+          <option value="RU">Nga</option>
+          <option value="LA">Lào</option>
+        </select>
+      </div>
+      <div>
+        <label for="jobLocations">Job locations</label>
+        <select multiple id="jobLocations" bind:value={formValues.jobLocations}>
+          <option value="VN">Việt Nam</option>
+          <option value="RU">Nga</option>
+          <option value="LA">Lào</option>
+        </select>
+      </div>
+    </form>
+  </div>
+</main>
+
+<style>
+  main {
+    margin: 0 auto;
+    display: flex;
+    gap: 1rem;
+    flex-direction: column;
+  }
+</style>
